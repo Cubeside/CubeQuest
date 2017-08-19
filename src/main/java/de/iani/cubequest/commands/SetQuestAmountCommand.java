41c96f851c548744e532c118ffc1dffd878ea051
Jonas Becker <jonas.becker@uni-muenster.de>
2017-08-19 03:18:00 +0200

    SetQuestAmountCommand hinzugefügt.

---
+++ src/main/java/de/iani/cubequest/commands/SetQuestAmountCommand.java
@@ -0,0 +1,48 @@
+package de.iani.cubequest.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+import de.iani.cubequest.CubeQuest;
+import de.iani.cubequest.QuestType;
+import de.iani.cubequest.quests.MaterialsAndAmountQuest;
+import de.iani.cubequest.quests.Quest;
+
+public class SetQuestAmountCommand extends SubCommand {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String alias, String commandString,
+            ArgsParser args) {
+
+        Quest quest = CubeQuest.getInstance().getQuestEditor().getEditingQuest(sender);
+        if (quest == null) {
+            CubeQuest.sendWarningMessage(sender, "Du bearbeitest derzeit keine Quest!");
+            return true;
+        }
+
+        if (!(quest instanceof MaterialsAndAmountQuest)) {
+            CubeQuest.sendWarningMessage(sender, "Diese Quest hat keine Anzahl, die gesetzt werden könnte.");
+            return true;
+        }
+
+        if (!args.hasNext()) {
+            CubeQuest.sendWarningMessage(sender, "Bitte gib die Anzahl an.");
+            return true;
+        }
+
+        int amount = args.getNext(-1);
+        if (amount < 0) {
+            CubeQuest.sendWarningMessage(sender, "Bitte gib die Anzahl als nicht-negative Ganzzahl an.");
+        }
+
+        ((MaterialsAndAmountQuest) quest).setAmount(amount);
+        CubeQuest.sendNormalMessage(sender, "Anzahl für " + QuestType.getQuestType(quest.getClass()) + " [" + quest.getId() + "] ist jetzt " + amount + ".");
+        return true;
+    }
+
+    @Override
+    public String getRequiredPermission() {
+        return CubeQuest.EDIT_QUESTS_PERMISSION;
+    }
+
+}
