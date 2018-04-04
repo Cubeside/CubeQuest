fe68807a9bd895ea7052ce0da4649c9b854614e5
Jonas Becker <jonas.becker@uni-muenster.de>
2018-04-04 19:51:12 +0200

    Improve SetRewardItemsCommand.

--- src/main/java/de/iani/cubequest/commands/SetRewardItemsCommand.java
+++ src/main/java/de/iani/cubequest/commands/SetRewardItemsCommand.java
@@ -4,19 +4,35 @@ import de.iani.cubequest.CubeQuest;
 import de.iani.cubequest.Reward;
 import de.iani.cubequest.quests.Quest;
 import de.iani.cubequest.util.ChatAndTextUtil;
+import de.iani.cubequest.util.ItemStackUtil;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
-public class SetRewardItemsCommand extends SubCommand {
+public class SetRewardItemsCommand extends SubCommand implements Listener {
     
     private boolean success;
     
+    private Set<UUID> currentlyEditing;
+    
     public SetRewardItemsCommand(boolean success) {
         this.success = success;
+        this.currentlyEditing = new HashSet<>();
+        Bukkit.getPluginManager().registerEvents(this, CubeQuest.getInstance());
+        CubeQuest.getInstance().getEventListener()
+                .addOnPlayerQuit(player -> this.currentlyEditing.remove(player.getUniqueId()));
     }
     
     @Override
@@ -29,11 +45,50 @@ public class SetRewardItemsCommand extends SubCommand {
         }
         
         Player player = (Player) sender; // sicher wegen requiresPlayer returns true
-        Reward formerReward = this.success ? quest.getSuccessReward() : quest.getFailReward();
-        ItemStack[] newContent = player.getInventory().getContents();
+        if (!this.currentlyEditing.add(player.getUniqueId())) {
+            ChatAndTextUtil.sendWarningMessage(sender, "Du bearbeitest bereits eine Belohnung.");
+            return true;
+        }
         
-        Reward resultReward =
-                new Reward(formerReward == null ? 0 : formerReward.getCubes(), newContent);
+        Reward formerReward = this.success ? quest.getSuccessReward() : quest.getFailReward();
+        Inventory inventory = Bukkit.createInventory(player, 27,
+                (this.success ? "Erfolgsbelohnung" : "Trostpreis") + " [Quest " + quest.getId()
+                        + "]");
+        if (formerReward != null) {
+            inventory.addItem(formerReward.getItems());
+        }
+        player.openInventory(inventory);
+        
+        return true;
+    }
+    
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onInventoryClosedEvent(InventoryCloseEvent event) {
+        if (!this.currentlyEditing.remove(event.getPlayer().getUniqueId())) {
+            return;
+        }
+        
+        Player player = (Player) event.getPlayer();
+        
+        Quest quest = CubeQuest.getInstance().getQuestEditor().getEditingQuest(player);
+        if (quest == null) {
+            ChatAndTextUtil.sendWarningMessage(player,
+                    "Du bearbeitest keine Quest mehr, kein Belohnung gesetzt!");
+            return;
+        }
+        
+        Reward formerReward = this.success ? quest.getSuccessReward() : quest.getFailReward();
+        int cubes = formerReward == null ? 0 : formerReward.getCubes();
+        int questPoints = formerReward == null ? 0 : formerReward.getQuestPoints();
+        int xp = formerReward == null ? 0 : formerReward.getXp();
+        
+        ItemStack[] items = ItemStackUtil.shrinkItemStack(event.getInventory().getContents());
+        event.getInventory().clear();
+        event.getInventory().addItem(items);
+        items = event.getInventory().getContents();
+        
+        Reward resultReward = new Reward(cubes, questPoints, xp, items);
+        
         if (this.success) {
             quest.setSuccessReward(resultReward);
         } else {
@@ -41,16 +96,14 @@ public class SetRewardItemsCommand extends SubCommand {
         }
         
         if (resultReward.isEmpty()) {
-            ChatAndTextUtil.sendNormalMessage(sender,
+            ChatAndTextUtil.sendNormalMessage(player,
                     (this.success ? "Erfolgsbelohnung" : "Trostpreis") + " für "
                             + quest.getTypeName() + " [" + quest.getId() + "] entfernt.");
         } else {
-            ChatAndTextUtil.sendNormalMessage(sender,
+            ChatAndTextUtil.sendNormalMessage(player,
                     "Items in " + (this.success ? "Erfolgsbelohnung" : "Trostpreis") + " für "
                             + quest.getTypeName() + " [" + quest.getId() + "] gesetzt.");
         }
-        
-        return true;
     }
     
     @Override
