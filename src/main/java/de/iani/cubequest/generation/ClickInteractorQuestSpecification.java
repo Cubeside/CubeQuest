3f93bbc0989a04177f308c1ea10292f895f8cc1a
Jonas Becker <jonas.becker@uni-muenster.de>
2018-06-02 23:05:18 +0200

    Slightly change hashCode of ClickInteractorQuestSpecification.

--- src/main/java/de/iani/cubequest/generation/ClickInteractorQuestSpecification.java
+++ src/main/java/de/iani/cubequest/generation/ClickInteractorQuestSpecification.java
@@ -129,8 +129,7 @@ public class ClickInteractorQuestSpecification extends DifficultyQuestSpecificat
     @Override
     public int hashCode() {
         long diffBits = Double.doubleToLongBits(getDifficulty());
-        int result = (int) (0xFFFFFFFFL & diffBits);
-        result ^= (int) (0xFFFFFFFFL & (diffBits >> 32));
+        int result = (int) (diffBits ^ (diffBits >>> 32));
         result = getInteractor().hashCode() + 31 * result;
         return result;
     }
