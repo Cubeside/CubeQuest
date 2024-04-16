ca4db4836d8e9a300ff2f2ea32513fdffd702174
Jonas Becker <jonas.becker@uni-muenster.de>
2024-04-16 18:38:56 +0200

    fix typo

--- src/main/java/de/iani/cubequest/commands/SetDeliveryInventoryCommand.java
+++ src/main/java/de/iani/cubequest/commands/SetDeliveryInventoryCommand.java
@@ -24,19 +24,19 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 public class SetDeliveryInventoryCommand extends SubCommand implements Listener {
-    
+
     public static final String COMMAND_PATH = "setDelivery";
     public static final String FULL_COMMAND = "quest " + COMMAND_PATH;
-    
+
     private Set<UUID> currentlyEditing;
-    
+
     public SetDeliveryInventoryCommand() {
         this.currentlyEditing = new HashSet<>();
         Bukkit.getPluginManager().registerEvents(this, CubeQuest.getInstance());
         CubeQuest.getInstance().getEventListener()
                 .addOnPlayerQuit(player -> this.currentlyEditing.remove(player.getUniqueId()));
     }
-    
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String alias, String commandString,
             ArgsParser args) {
@@ -45,69 +45,69 @@ public class SetDeliveryInventoryCommand extends SubCommand implements Listener 
             ChatAndTextUtil.sendWarningMessage(sender, "Du bearbeitest derzeit keine Quest!");
             return true;
         }
-        
+
         if (!(quest instanceof DeliveryQuest)) {
             ChatAndTextUtil.sendWarningMessage(sender, "Diese Quest erfordert keine Lieferung.");
             return true;
         }
-        
+
         Player player = (Player) sender; // sicher wegen requiresPlayer returns true
         if (!this.currentlyEditing.add(player.getUniqueId())) {
             ChatAndTextUtil.sendWarningMessage(sender, "Du bearbeitest bereits eine Lieferung.");
             return true;
         }
-        
-        Inventory inventory = Bukkit.createInventory(player, 27, ("Lierefungsumfang [Quest " + quest.getId() + "]"));
+
+        Inventory inventory = Bukkit.createInventory(player, 27, ("Lieferungsumfang [Quest " + quest.getId() + "]"));
         inventory.addItem(((DeliveryQuest) quest).getDelivery());
         player.openInventory(inventory);
-        
+
         return true;
     }
-    
+
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onInventoryClosedEvent(InventoryCloseEvent event) {
         if (!this.currentlyEditing.remove(event.getPlayer().getUniqueId())) {
             return;
         }
-        
+
         Player player = (Player) event.getPlayer();
-        
+
         Quest quest = CubeQuest.getInstance().getQuestEditor().getEditingQuest(player);
         if (quest == null) {
             ChatAndTextUtil.sendWarningMessage(player, "Du bearbeitest keine Quest mehr, keine Lieferung gesetzt!");
             return;
         }
-        
+
         if (!(quest instanceof DeliveryQuest)) {
             ChatAndTextUtil.sendWarningMessage(player,
                     "Du bearbeitest keine Lieferungsquest mehr, keine Lieferung gesetzt!");
             return;
         }
-        
+
         ItemStack[] items = ItemStacks.shrink(event.getInventory().getContents());
         event.getInventory().clear();
         event.getInventory().addItem(items);
         items = event.getInventory().getContents();
-        
+
         ((DeliveryQuest) quest).setDelivery(items);
-        
+
         ChatAndTextUtil.sendNormalMessage(player,
                 "Lieferungsumfang für " + quest.getTypeName() + " [" + quest.getId() + "] gesetzt.");
     }
-    
+
     @Override
     public boolean requiresPlayer() {
         return true;
     }
-    
+
     @Override
     public String getRequiredPermission() {
         return CubeQuest.EDIT_QUESTS_PERMISSION;
     }
-    
+
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, ArgsParser args) {
         return Collections.emptyList();
     }
-    
+
 }
