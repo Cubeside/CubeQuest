db2805f22b91d3f57886831e8e738cdc1e49bf1f
Jonas Becker <jonas.becker@uni-muenster.de>
2024-11-22 22:14:36 +0100

    maybe fix color in renamed conditions

--- src/main/java/de/iani/cubequest/conditions/RenamedCondition.java
+++ src/main/java/de/iani/cubequest/conditions/RenamedCondition.java
@@ -8,58 +8,57 @@ import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.ComponentBuilder.FormatRetention;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.entity.Player;
 
 
 public class RenamedCondition extends QuestCondition {
-    
+
     private String text;
     private QuestCondition original;
-    
+
     public static QuestCondition rename(String text, QuestCondition original) {
-        original = (original instanceof RenamedCondition) ? ((RenamedCondition) original).original
-                : original;
+        original = (original instanceof RenamedCondition) ? ((RenamedCondition) original).original : original;
         return text.isEmpty() ? original : new RenamedCondition(text, original);
     }
-    
+
     private RenamedCondition(String text, QuestCondition original) {
         super(true);
         init(text, original);
     }
-    
+
     public RenamedCondition(Map<String, Object> serialized) {
         super(true);
         init((String) serialized.get("text"), (QuestCondition) serialized.get("original"));
     }
-    
+
     private void init(String text, QuestCondition original) {
         this.text = Objects.requireNonNull(text);
         this.original = Objects.requireNonNull(original);
     }
-    
+
     @Override
     public boolean fulfills(Player player, PlayerData data) {
         return this.original.fulfills(player, data);
     }
-    
+
     @Override
     public BaseComponent[] getConditionInfo() {
-        return new ComponentBuilder(this.text).create();
+        return new BaseComponent[] {TextComponent.fromLegacy(this.text)};
     }
-    
+
     @Override
     public BaseComponent[] getConditionInfo(boolean includeHiddenInfo) {
         BaseComponent[] result = getConditionInfo();
         if (!includeHiddenInfo) {
             return result;
         }
-        
-        return new ComponentBuilder("").append(result).append(" (Intern: ").reset()
-                .color(ChatColor.DARK_AQUA).append(this.original.getConditionInfo(true))
-                .retain(FormatRetention.NONE).append(")").reset().color(ChatColor.DARK_AQUA)
-                .create();
+
+        return new ComponentBuilder("").append(result).append(" (Intern: ").reset().color(ChatColor.DARK_AQUA)
+                .append(this.original.getConditionInfo(true)).retain(FormatRetention.NONE).append(")").reset()
+                .color(ChatColor.DARK_AQUA).create();
     }
-    
+
     @Override
     public Map<String, Object> serialize() {
         Map<String, Object> result = new HashMap<>();
@@ -67,5 +66,5 @@ public class RenamedCondition extends QuestCondition {
         result.put("original", this.original);
         return result;
     }
-    
+
 }
