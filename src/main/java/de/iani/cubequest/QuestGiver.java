d87b392e199cb348693ffb4abc6b38b20b1c80cf
Jonas Becker <jonas.becker@uni-muenster.de>
2024-11-23 13:25:44 +0100

    missed a method

--- src/main/java/de/iani/cubequest/QuestGiver.java
+++ src/main/java/de/iani/cubequest/QuestGiver.java
@@ -24,6 +24,7 @@ import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.chat.hover.content.Text;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
@@ -35,35 +36,35 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
 
 public class QuestGiver implements InteractorProtecting, ConfigurationSerializable {
-    
+
     private Interactor interactor;
     private String name;
     private boolean reactIfNoQuest;
-    
+
     private Set<Quest> quests;
     private Map<UUID, Set<Quest>> mightGetFromHere;
-    
+
     private long lastSavedCache;
     private int forceSaveTaskId;
-    
+
     public QuestGiver(Interactor interactor, String name) {
         Verify.verify(Util.isSafeGiverName(name));
         Verify.verify(interactor != null /* && interactor.isLegal()t */);
-        
+
         this.interactor = interactor;
         this.name = name;
         this.reactIfNoQuest = false;
-        
+
         this.quests = new HashSet<>();
         this.mightGetFromHere = new HashMap<>();
-        
+
         saveConfig();
     }
-    
+
     @SuppressWarnings("unchecked")
     public QuestGiver(Map<String, Object> serialized) throws InvalidConfigurationException {
         this.mightGetFromHere = new HashMap<>();
-        
+
         try {
             this.interactor = (Interactor) serialized.get("interactor");
             if (this.interactor == null/* || !this.interactor.isLegal() */) {
@@ -86,33 +87,38 @@ public class QuestGiver implements InteractorProtecting, ConfigurationSerializab
             throw new InvalidConfigurationException(e);
         }
     }
-    
+
     @Override
     public Interactor getInteractor() {
         return this.interactor;
     }
-    
+
     public String getName() {
         return this.name;
     }
-    
+
+    @Override
+    public BaseComponent[] getProtectingInfo() {
+        return new BaseComponent[] {new TextComponent("QuestGiver " + getName())};
+    }
+
     public boolean isReactIfNoQuest() {
         return this.reactIfNoQuest;
     }
-    
+
     public void setReactIfNoQuest(boolean react) {
         this.reactIfNoQuest = react;
         saveConfig();
     }
-    
+
     public Set<Quest> getQuests() {
         return Collections.unmodifiableSet(this.quests);
     }
-    
+
     public boolean hasQuest(Quest quest) {
         return this.quests.contains(quest);
     }
-    
+
     public boolean addQuest(Quest quest) {
         if (this.quests.add(quest)) {
             saveConfig();
@@ -120,11 +126,11 @@ public class QuestGiver implements InteractorProtecting, ConfigurationSerializab
         }
         return false;
     }
-    
+
     public boolean removeQuest(Quest quest) {
         if (this.quests.remove(quest)) {
             saveConfig();
-            
+
             Iterator<Set<Quest>> it = this.mightGetFromHere.values().iterator();
             while (it.hasNext()) {
                 Set<Quest> set = it.next();
@@ -136,26 +142,26 @@ public class QuestGiver implements InteractorProtecting, ConfigurationSerializab
         }
         return false;
     }
-    
+
     public boolean hasQuestFoPlayer(Player player) {
         return hasQuestForPlayer(player, CubeQuest.getInstance().getPlayerData(player));
     }
-    
+
     public boolean hasQuestForPlayer(Player player, PlayerData playerData) {
         for (Quest quest : this.quests) {
             if (quest.fulfillsGivingConditions(player, playerData)) {
                 return true;
             }
         }
-        
+
         return false;
     }
-    
+
     public boolean mightGetFromHere(Player player, Quest quest) {
         Set<Quest> set = this.mightGetFromHere.get(player.getUniqueId());
         return set == null ? false : set.contains(quest);
     }
-    
+
     public boolean addMightGetFromHere(Player player, Quest quest) {
         Set<Quest> set = this.mightGetFromHere.get(player.getUniqueId());
         if (set == null) {
@@ -164,7 +170,7 @@ public class QuestGiver implements InteractorProtecting, ConfigurationSerializab
         }
         return set.add(quest);
     }
-    
+
     public boolean removeMightGetFromHere(Player player, Quest quest) {
         Set<Quest> set = this.mightGetFromHere.get(player.getUniqueId());
         if (set == null) {
@@ -176,35 +182,35 @@ public class QuestGiver implements InteractorProtecting, ConfigurationSerializab
         }
         return result;
     }
-    
+
     public boolean removeMightGetFromHere(Player player) {
         Set<Quest> set = this.mightGetFromHere.remove(player.getUniqueId());
         return set != null && !set.isEmpty();
     }
-    
+
     public boolean showQuestsToPlayer(Player player) {
         if (!player.hasPermission(CubeQuest.ACCEPT_QUESTS_PERMISSION)) {
             return false;
         }
-        
+
         List<Quest> givables = new ArrayList<>();
         PlayerData playerData = CubeQuest.getInstance().getPlayerData(player);
         this.quests.stream().filter(q -> q.fulfillsGivingConditions(player, playerData)).forEach(q -> givables.add(q));
         givables.sort(Quest.QUEST_DISPLAY_COMPARATOR);
-        
+
         List<Quest> teasers = new ArrayList<>();
         this.quests.stream()
                 .filter(q -> q.getVisibleGivingConditions().stream().anyMatch(c -> !c.fulfills(player, playerData)))
                 .forEach(q -> teasers.add(q));
-        
+
         if (!this.reactIfNoQuest && givables.isEmpty() && teasers.isEmpty()) {
             return false;
         }
-        
+
         ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
         BookMeta meta = (BookMeta) book.getItemMeta();
         meta.setTitle("Quests");
-        
+
         if (givables.isEmpty() && teasers.isEmpty()) {
             ComponentBuilder builder = new ComponentBuilder("");
             builder.append("Leider habe ich keine neuen Aufgaben für dich.").bold(true).color(ChatColor.GOLD);
@@ -215,48 +221,48 @@ public class QuestGiver implements InteractorProtecting, ConfigurationSerializab
         } else {
             for (Quest q : givables) {
                 List<BaseComponent[]> displayMessageList = ChatAndTextUtil.getQuestDescription(q);
-                
+
                 ComponentBuilder builder = new ComponentBuilder("");
                 ClickEvent cEvent = new ClickEvent(ClickEvent.Action.RUN_COMMAND,
                         "/quest acceptQuest " + this.name + " " + q.getId());
                 HoverEvent hEvent = new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Hier klicken"));
                 builder.append("Quest annehmen").color(ChatColor.DARK_GREEN).bold(true).event(cEvent).event(hEvent);
                 displayMessageList.add(builder.create());
-                
+
                 ChatAndTextUtil.writeIntoBook(meta, displayMessageList);
-                
+
                 addMightGetFromHere(player, q);
             }
-            
+
             for (Quest q : teasers) {
                 List<BaseComponent[]> displayMessageList = ChatAndTextUtil.getQuestDescription(q, true, player);
                 ChatAndTextUtil.writeIntoBook(meta, displayMessageList);
             }
         }
-        
+
         meta.setAuthor(getName());
         book.setItemMeta(meta);
         player.openBook(book);
-        
+
         return true;
     }
-    
+
     @Override
     public boolean onInteractorDamagedEvent(InteractorDamagedEvent<?> event) {
         if (event.getInteractor().equals(this.interactor)) {
             event.setCancelled(true);
             return true;
         }
-        
+
         return false;
     }
-    
+
     @Override
     public void onCacheChanged() {
         if (System.currentTimeMillis() - this.lastSavedCache >= 5 * 60 * 1000) {
             saveConfig();
             this.lastSavedCache = System.currentTimeMillis();
-            
+
             if (this.forceSaveTaskId >= 0) {
                 Bukkit.getScheduler().cancelTask(this.forceSaveTaskId);
                 this.forceSaveTaskId = -1;
@@ -269,22 +275,22 @@ public class QuestGiver implements InteractorProtecting, ConfigurationSerializab
             }, 5 * 60 * 20);
         }
     }
-    
+
     @Override
     public Map<String, Object> serialize() {
         Map<String, Object> result = new HashMap<>();
-        
+
         result.put("interactor", this.interactor);
         result.put("name", this.name);
         result.put("reactIfNoQuest", this.reactIfNoQuest);
-        
+
         List<Integer> questIdList = new ArrayList<>();
         this.quests.forEach(q -> questIdList.add(q.getId()));
         result.put("quests", questIdList);
-        
+
         return result;
     }
-    
+
     public void saveConfig() {
         File folder = new File(CubeQuest.getInstance().getDataFolder(), "questGivers");
         folder.mkdirs();
@@ -297,5 +303,5 @@ public class QuestGiver implements InteractorProtecting, ConfigurationSerializab
             CubeQuest.getInstance().getLogger().log(Level.SEVERE, "Could not save QuestGiver.", e);
         }
     }
-    
+
 }
