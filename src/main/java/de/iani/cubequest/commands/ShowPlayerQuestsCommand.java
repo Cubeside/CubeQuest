a2b811e0b0602f8b794e1a83e4f6656eb0eee692
Jonas Becker <j_beck64@wwu.de>
2025-08-11 21:54:13 +0200

    slight change in hover events

--- src/main/java/de/iani/cubequest/commands/ShowPlayerQuestsCommand.java
+++ src/main/java/de/iani/cubequest/commands/ShowPlayerQuestsCommand.java
@@ -34,9 +34,9 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
 
 public class ShowPlayerQuestsCommand extends SubCommand {
-
+    
     public enum ListType {
-
+        
         ALL(CubeQuest.SEE_PLAYER_INFO_PERMISSION, "showAllQuests", "", false, (pd, id) -> true),
         NOTGIVENTO(CubeQuest.SEE_PLAYER_INFO_PERMISSION, "showMissingQuests", "fehlende", false,
                 (pd, id) -> pd.getPlayerStatus(id) == Status.NOTGIVENTO,
@@ -55,7 +55,7 @@ public class ShowPlayerQuestsCommand extends SubCommand {
                 (pd, id) -> pd.getPlayerStatus(id) == Status.FAIL),
         FROZEN(CubeQuest.ACCEPT_QUESTS_PERMISSION, "showFrozenQuests", "eingefrorene", true,
                 (pd, id) -> pd.getPlayerStatus(id) == Status.FROZEN);
-
+        
         public final String permission;
         public final String commandPath;
         public final String fullCommand;
@@ -63,7 +63,7 @@ public class ShowPlayerQuestsCommand extends SubCommand {
         public final boolean hasState;
         public final BiPredicate<PlayerData, Integer> displayPredicate;
         Function<PlayerData, Stream<Quest>> questSupplier;
-
+        
         private ListType(String permission, String commandPath, String attribute, boolean hasState,
                 BiPredicate<PlayerData, Integer> displayPredicate, Function<PlayerData, Stream<Quest>> questSupplier) {
             this.permission = permission;
@@ -74,38 +74,38 @@ public class ShowPlayerQuestsCommand extends SubCommand {
             this.displayPredicate = displayPredicate;
             this.questSupplier = questSupplier;
         }
-
+        
         private ListType(String permission, String commandPath, String attribute, boolean hasState,
                 BiPredicate<PlayerData, Integer> displayPredicate) {
             this(permission, commandPath, attribute, hasState, displayPredicate,
                     pd -> QuestManager.getInstance().getQuests().stream());
         }
     }
-
+    
     private static final int MAX_NUM_PAGES_QUEST_LIST = 30;
-
+    
     private ListType type;
-
+    
     public ShowPlayerQuestsCommand(ListType type) {
         this.type = type;
     }
-
+    
     @SuppressWarnings("null")
     @Override
     public boolean onCommand(CommandSender sender, Command command, String alias, String commandString,
             ArgsParser args) {
-
+        
         OfflinePlayer player;
-
+        
         if (args.remaining() > 0 && !args.seeNext("").startsWith(".")) {
             if (!sender.hasPermission(CubeQuest.SEE_PLAYER_INFO_PERMISSION)) {
                 ChatAndTextUtil.sendNoPermissionMessage(sender);
                 return true;
             }
-
+            
             String playerName = args.next();
             player = CubeQuest.getInstance().getPlayerUUIDCache().getPlayer(playerName);
-
+            
             if (player == null) {
                 ChatAndTextUtil.sendWarningMessage(sender, "Spieler " + playerName + " nicht gefunden.");
                 return true;
@@ -113,23 +113,23 @@ public class ShowPlayerQuestsCommand extends SubCommand {
         } else {
             player = (Player) sender;
         }
-
+        
         List<Quest> showableQuests = new ArrayList<>();
         PlayerData playerData = CubeQuest.getInstance().getPlayerData(player);
         Stream<Quest> questStream = this.type.questSupplier.apply(playerData);
         questStream = questStream.filter(q -> q.isVisible() && this.type.displayPredicate.test(playerData, q.getId()));
         questStream.forEach(q -> showableQuests.add(q));
         showableQuests.sort(Quest.QUEST_DISPLAY_COMPARATOR);
-
+        
         ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
         List<BookMeta> books = new ArrayList<>();
         List<String> firstQuestsInBooks = new ArrayList<>();
         BookMeta meta = null;
         boolean oneBookEnough = true;
-
+        
         boolean isActiveAndHasHidden =
                 this.type == ListType.ACTIVE && playerData.getActiveQuests().stream().anyMatch(qs -> qs.isHidden());
-
+        
         if (showableQuests.isEmpty()) {
             meta = (BookMeta) book.getItemMeta();
             ComponentBuilder builder = new ComponentBuilder("");
@@ -149,39 +149,49 @@ public class ShowPlayerQuestsCommand extends SubCommand {
         } else {
             for (Quest q : showableQuests) {
                 List<BaseComponent[]> displayMessageList = ChatAndTextUtil.getQuestDescription(q);
-
-                HoverEvent hoverEvent = new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Hier klicken"));
                 if (this.type.hasState) {
+                    HoverEvent stateHoverEvent =
+                            new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Fortschritt anzeigen"));
                     ClickEvent stateClickEvent = new ClickEvent(ClickEvent.Action.RUN_COMMAND,
                             "/quest stateInfo " + (player == sender ? "" : (player.getName() + " ")) + q.getId());
+                    HoverEvent giveMessageHoverEvent =
+                            new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Vergabenachricht anzeigen"));
                     ClickEvent giveMessageClickEvent = new ClickEvent(ClickEvent.Action.RUN_COMMAND,
                             "/quest showGiveMessage " + (player == sender ? "" : (player.getName() + " ")) + q.getId());
-
-                    displayMessageList.add(new ComponentBuilder("").append("Fortschritt anzeigen\n")
-                            .color(ChatColor.DARK_GREEN).bold(true).event(stateClickEvent).event(hoverEvent).create());
+                    
+                    displayMessageList
+                            .add(new ComponentBuilder("").append("Fortschritt anzeigen\n").color(ChatColor.DARK_GREEN)
+                                    .bold(true).event(stateClickEvent).event(stateHoverEvent).create());
                     displayMessageList.add(
                             new ComponentBuilder("").append("Vergabenachricht anzeigen\n").color(ChatColor.DARK_GREEN)
-                                    .bold(true).event(giveMessageClickEvent).event(hoverEvent).create());
+                                    .bold(true).event(giveMessageClickEvent).event(giveMessageHoverEvent).create());
                 }
                 if (this.type == ListType.ACTIVE && player == sender) {
+                    HoverEvent hideHoverEvent =
+                            new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Quest ausblenden"));
                     ClickEvent hideClickEvent = new ClickEvent(ClickEvent.Action.RUN_COMMAND,
                             "/" + HideOrRestoreQuestCommand.HIDE_FULL_COMMAND + " " + q.getId());
-                    displayMessageList.add(new ComponentBuilder("").append("Quest ausblenden\n")
-                            .color(ChatColor.DARK_GREEN).bold(true).event(hideClickEvent).event(hoverEvent).create());
+                    displayMessageList
+                            .add(new ComponentBuilder("").append("Quest ausblenden\n").color(ChatColor.DARK_GREEN)
+                                    .bold(true).event(hideClickEvent).event(hideHoverEvent).create());
                 } else if (this.type == ListType.HIDDEN && player == sender) {
+                    HoverEvent restoreHoverEvent =
+                            new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Quest einblenden"));
                     ClickEvent restoreClickEvent = new ClickEvent(ClickEvent.Action.RUN_COMMAND,
                             "/" + HideOrRestoreQuestCommand.RESTORE_FULL_COMMAND + " " + q.getId());
                     displayMessageList
                             .add(new ComponentBuilder("").append("Quest einblenden\n").color(ChatColor.DARK_GREEN)
-                                    .bold(true).event(restoreClickEvent).event(hoverEvent).create());
+                                    .bold(true).event(restoreClickEvent).event(restoreHoverEvent).create());
                 }
                 if (sender.hasPermission(CubeQuest.EDIT_QUESTS_PERMISSION)) {
+                    HoverEvent infoHoverEvent = new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Info anzeigen"));
                     ClickEvent infoClickEvent = new ClickEvent(ClickEvent.Action.RUN_COMMAND,
                             "/" + QuestInfoCommand.FULL_COMMAND + " " + q.getId());
-                    displayMessageList.add(new ComponentBuilder("").append("Info anzeigen\n")
-                            .color(ChatColor.DARK_GREEN).bold(true).event(infoClickEvent).event(hoverEvent).create());
+                    displayMessageList
+                            .add(new ComponentBuilder("").append("Info anzeigen\n").color(ChatColor.DARK_GREEN)
+                                    .bold(true).event(infoClickEvent).event(infoHoverEvent).create());
                 }
-
+                
                 if (meta == null
                         || !ChatAndTextUtil.writeIntoBook(meta, displayMessageList, MAX_NUM_PAGES_QUEST_LIST)) {
                     meta = (BookMeta) book.getItemMeta();
@@ -191,11 +201,11 @@ public class ShowPlayerQuestsCommand extends SubCommand {
                     oneBookEnough &= books.size() == 1;
                 }
             }
-
+            
             if (!oneBookEnough) {
                 meta = (BookMeta) book.getItemMeta();
             }
-
+            
             List<BaseComponent[]> toc = new ArrayList<>();
             if (isActiveAndHasHidden) {
                 ComponentBuilder builder = new ComponentBuilder("");
@@ -215,7 +225,7 @@ public class ShowPlayerQuestsCommand extends SubCommand {
                     toc.add(null);
                 }
             }
-
+            
             if (!oneBookEnough) {
                 int bookIndex = -1;
                 if (args.hasNext()) {
@@ -238,7 +248,7 @@ public class ShowPlayerQuestsCommand extends SubCommand {
                         return true;
                     }
                 }
-
+                
                 if (bookIndex == -1) {
                     toc.add(new ComponentBuilder("Buchliste:").bold(true).create());
                     for (int i = 0; i < books.size(); i++) {
@@ -263,38 +273,38 @@ public class ShowPlayerQuestsCommand extends SubCommand {
                 ChatAndTextUtil.sendWarningMessage(sender, "Deine Quest-Liste hat nur eine Seite.");
             }
         }
-
+        
         meta.setTitle("Quests");
         meta.setAuthor(CubeQuest.PLUGIN_TAG);
         book.setItemMeta(meta);
         ((Player) sender).openBook(book);
-
+        
         return true;
     }
-
+    
     @Override
     public String getRequiredPermission() {
         return this.type.permission;
     }
-
+    
     @Override
     public boolean requiresPlayer() {
         return true;
     }
-
+    
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, ArgsParser args) {
         if (!sender.hasPermission(CubeQuest.SEE_PLAYER_INFO_PERMISSION)) {
             return Collections.emptyList();
         }
-
+        
         return ChatAndTextUtil.polishTabCompleteList(Bukkit.getOnlinePlayers().stream().map(p -> p.getName())
                 .collect(Collectors.toCollection(() -> new ArrayList<>())), args.getNext(""));
     }
-
+    
     @Override
     public String getUsage() {
         return "(zeigt deine " + (this.type.attribute.isEmpty() ? "" : (this.type.attribute + "n ")) + "Quests an)";
     }
-
+    
 }
